21d25c06ae1463fce52b2e32aa60945d02660199
Neradovskiy <bopoh143@gmail.com>
2025-06-29 10:07:01 +0500

    Add task2 solution

---
+++ task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+	internal class Program
+	{
+		static int RangeSumm(int num, int hightRange)
+		{
+			int result = 0;
+			for (int i = num; i < hightRange; i++)
+				result += i;
+			return result;
+		}
+		static void Main(string[] args)
+		{
+			int hightRange = 500;
+			int num = int.Parse(Console.ReadLine());
+			Console.WriteLine(RangeSumm(num, hightRange));
+		}
+	}
+}
