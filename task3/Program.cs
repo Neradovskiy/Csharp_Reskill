37fb3456430b782438dcb5c5e802a3b170999bdd
Neradovskiy <bopoh143@gmail.com>
2025-06-30 11:29:34 +0500

    added task3 description and solution

---
+++ task3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			try
+			{
+				Console.WriteLine("Enter x :");
+				double x = double.Parse(Console.ReadLine());
+				Console.WriteLine("Enter y :");
+				double y = double.Parse(Console.ReadLine());
+				Console.WriteLine("Result: [" + Math.Pow(x, y) + "]");
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.ToString());
+			}
+		}
+	}
+}
+
