1e600489ee3f76d0ec4c45b6baab00672b7c1d27
Neradovskiy <bopoh143@gmail.com>
2025-07-01 10:21:36 +0500

    add week2 task4 description and solution

---
+++ week2Task4/task4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+	internal class Program
+	{
+		//returns the product of all integers from a to range if a < 1 return -1
+		static int MultiplyRange (int a, uint range)
+		{
+			if (a < 1)
+				return -1;
+			int result = a;
+			for (; a < range; a++)
+			{
+				result *=a;
+			}
+			return result;
+		}
+		static void Main(string[] args)
+		{
+			uint range = 21;
+			int a = int.Parse(Console.ReadLine());
+			Console.WriteLine(MultiplyRange(a, range));
+		}
+	}
+}
