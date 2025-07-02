982fd23eaf692ca8603f8c4e335ccfcaee1fdf91
Neradovskiy <bopoh143@gmail.com>
2025-07-02 11:04:06 +0500

    add task5 solution

---
+++ week2/task5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task5
+{
+	internal class Program
+	{
+		static int[] MultiplicationTable(int a)
+		{
+			int[] result = new int[11];
+			for (int i = 0; i <= 10; ++i) 
+			 result[i] = a * i;
+			return result;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter factor");
+			int factor = int.Parse(Console.ReadLine());
+			int[] table = MultiplicationTable(factor);
+			for (int i = 0; i < table.Length; ++i) 
+				Console.WriteLine(factor + " x " + i + " = " + table[i]);
+		}
+	}
+}
