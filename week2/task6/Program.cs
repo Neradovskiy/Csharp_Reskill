339611af3bf9a0a8e99e4ef5f1c314d7ba40a300
Neradovskiy <bopoh143@gmail.com>
2025-07-03 11:30:04 +0500

    add task6 description and solution

---
+++ week2/task6/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter max num: ");
+			int maxNum = int.Parse(Console.ReadLine());
+			for (int i = 0; i <= maxNum; i++) 
+				Console.WriteLine(i);
+		}
+	}
+}
