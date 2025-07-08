c067ee04f54e510242851e468ddfc285ea6d5eb6
Neradovskiy <bopoh143@gmail.com>
2025-07-08 15:51:11 +0500

    add task8 description and solution

---
+++ week2/task8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task8
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter num1: ");
+			int num1 = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter num2: ");
+			int num2 = int.Parse(Console.ReadLine());
+			if (num1 > num2)
+				(num1, num2) = (num2, num1);
+			int result = 0;
+			for (int i = num1; i <= num2; i++)
+				result += i;
+			Console.WriteLine(result);
+		}
+	}
+}
