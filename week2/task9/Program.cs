30bfedbd6d408c586896e0eb8b5c463e3ec66b12
Neradovskiy <bopoh143@gmail.com>
2025-07-12 11:51:47 +0500

    add task9 description and solution

---
+++ week2/task9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task9
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter num: ");
+			int buff = 0;
+			int result = 0;
+			do
+			{
+				Console.WriteLine("Please enter 0 to get the result");
+				Console.WriteLine("Enter num: ");
+				buff = int.Parse(Console.ReadLine());
+				result += buff;
+			} while (buff != 0);
+			Console.WriteLine("Result is: " + "[" + result + "]");
+		}
+	}
+}
