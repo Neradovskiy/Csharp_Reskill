c2683de2c9e5ed17bb22ab407e117d4280d3afd0
Neradovskiy <bopoh143@gmail.com>
2025-07-13 12:55:20 +0500

    add week3 task1 description and solution

---
+++ week3/task1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+	internal class Program
+	{
+		static bool isIdenticalNum(int num)
+		{
+			string buff = num.ToString();
+			int index = 0;
+			do
+			{
+				for (int i = index + 1; i < buff.Length; ++i)
+				{
+					if (buff[index] == buff[i])
+						return true;
+				}
+				index++;
+			}
+			while (index < buff.Length);
+			return false;
+		}
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter start num");
+			int startNum = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Enter end num");
+			int endNum = int.Parse(Console.ReadLine());
+
+			int result = 0;
+			for (int i = startNum; i <= endNum; ++i)
+			{
+				if (isIdenticalNum(i))
+				{
+					++result;
+				}
+			}
+			Console.WriteLine(result);
+		}
+	}
+}
