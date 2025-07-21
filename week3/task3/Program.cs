6d4f5e82fba9b3f1997ac6f46fd4d0a2891b5cca
Neradovskiy <bopoh143@gmail.com>
2025-07-21 12:17:14 +0500

    add week3 task3 description and solution

---
+++ week3/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task3
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter num: ");
+			string numStr = Console.ReadLine();
+			string result = numStr.Replace("3", "").Replace("6", "");
+
+			Console.WriteLine(result);
+		}
+	}
+}
