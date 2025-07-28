30c19fedd12dd1946dfc4d303d3e6b19257f92db
Neradovskiy <bopoh143@gmail.com>
2025-07-28 09:57:21 +0500

    add week3 task4 description and solution

---
+++ week3/task4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task4
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int num = int.Parse(Console.ReadLine());
+			if (num < 0)
+				num = -num;
+			for (int i = 1; i <= num; i++)
+			{
+				int result = i * i;
+				if (num % result == 0 && num % result * i != 0)
+					Console.WriteLine(i);
+			}
+		}
+	}
+}
