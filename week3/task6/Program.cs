a7adf52f76b6a77cf0fec97231e744644a96d710
Neradovskiy <bopoh143@gmail.com>
2025-08-05 16:24:22 +0500

    add week3 task6 description and solution

---
+++ week3/task6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task6
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter num: ");
+			int num = int.Parse(Console.ReadLine());
+			int i = 1;
+			while (i <= num)
+			{
+				if (num % i == 0)
+					Console.WriteLine(i);
+				++i;
+			}
+		}
+	}
+}
