a073b62955b7dc686b5f26f944ac3ff9558b4b17
Neradovskiy <bopoh143@gmail.com>
2025-11-28 17:46:38 +0500

    added task3 solution

---
+++ week4/task3/task3.cs
@@ -0,0 +1,8 @@
+﻿const int arrSize = 10;
+int[] arr = new int[arrSize];
+Random rnd = new Random();
+for (int i = 0; i < arrSize; i++)
+    arr[i] = rnd.Next(0, 10);
+Console.WriteLine(string.Join(", ", arr));
+Console.WriteLine("Max value is: " + arr.Max());
+Console.WriteLine("Min value is: " + arr.Min());
